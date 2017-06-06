862517c49ee96686e22382a808fae113a64bde1a
aadarshtn <aadarshtn@gmail.com>
2017-06-07 03:48:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ var element = document.getElementById('main-text');
 element.innerHTML = ('New Value');
 
 console.log('Work Doe');
+
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+  img.style.marginleft ="100px";  
+};
