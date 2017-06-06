9ea4f022541a1ad611318561bb84ef425f6d7104
aadarshtn <aadarshtn@gmail.com>
2017-06-07 01:36:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,41 +22,50 @@ var articleOne = {
 }
 ;
 
-var htmltemplate = 
-`<html>
-  <head>
-   <title>
-    ${title}
-   </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-      <div class="container">
-        <div>
-            <a href="/">Home</a>
-            <hr/>
-            ${heading}
-            <div>
-               ${date}
-            </div>
-             <div>
-               ${reporter}
-             </div>
-              <div>
-              ${content}
-              </div>
-          </div>
-      </div>
-    </body>
-</html>`
-;
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var reporter = data.reporter;
+    var content = data.content;
+
+    var htmlTemplate = 
+                    `<html>
+                      <head>
+                       <title>
+                        ${title}
+                       </title>
+                            <meta name="viewport" content="width=device-width, initial-scale=1" />
+                            <link href="/ui/style.css" rel="stylesheet" />
+                        </head>
+                        <body>
+                          <div class="container">
+                            <div>
+                                <a href="/">Home</a>
+                                <hr/>
+                                ${heading}
+                                <div>
+                                   ${date}
+                                </div>
+                                 <div>
+                                   ${reporter}
+                                 </div>
+                                  <div>
+                                  ${content}
+                                  </div>
+                              </div>
+                          </div>
+                        </body>
+                    </html>`
+                    ;
+                    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
