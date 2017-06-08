f7a9d73214dc24352af0c5073b41c5ca60c34703
aadarshtn <aadarshtn@gmail.com>
2017-06-09 00:55:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,8 +112,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var counter= 0;
-app.get('/counter' , function(req,res) {
-    counter = counter+1;
+app.get('/counter' , function(req,res)
+{
+    counter = counter +1;
     res.send(counter.toString());
 });
 
