f4bd2dd037b4e3ac81ac61812fc7bfff440f07dc
aadarshtn <aadarshtn@gmail.com>
2017-06-08 13:10:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = ('New Value');
-
-console.log('Work Doe');
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function()
-{
-  var interval = setInterval(moveRight,25);
-};
