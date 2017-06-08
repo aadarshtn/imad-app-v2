0547f0a30d0f160493850dc491f5748f20986502
aadarshtn <aadarshtn@gmail.com>
2017-06-09 01:40:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,11 @@ function createTemplate(data) {
                     return htmlTemplate;
 }
 
+
+app.get('/counter', function( req, res) {
+    counter = counter +1;
+    res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -107,11 +112,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/counter', function( req, res) {
-    counter = counter +1;
-    res.send(counter.toString());
 });
 
 
