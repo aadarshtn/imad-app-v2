0e490e81c8bf258fd41b5aa84d8bd3046f315493
aadarshtn <aadarshtn@gmail.com>
2017-06-09 01:59:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var button= document.getElementById('counter');
+var span = document.getElementById('span');
+
+button.Onclick= function (){
+    counter = counter + 1;
+    span.InnerHtml = counter.toString();
+};
