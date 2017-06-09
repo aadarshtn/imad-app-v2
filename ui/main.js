c2e0d2365875a5794926215323b4c3111202ce87
aadarshtn <aadarshtn@gmail.com>
2017-06-09 13:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,33 @@
 console.log('Loaded');
 
+//Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function(){
-    counter = counter +1;
-    var span= document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    //Create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and keep it in a variable
+    
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Do some action
+          if (request.status === 200); {
+              
+            // Render the variable in the correct span
+              
+              var counter = request.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      } 
+    };
+    
+//Create the request code
+request.open('http://aadarshtn.imad.hasura-app.io/counter', true);
+request.send(null);
+
 };
     
